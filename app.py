47fd194f6e368ad531c718e96de819b6e44e7802
立花卓遠 <takuo.tachibana@leadx.co.jp>
2023-02-25 22:00:06 +0900

    修正

--- app.py
+++ app.py
@@ -10,8 +10,18 @@ from warning.warning import warning_line
 #赤
 color = (255, 0, 0) 
 
+def get_style():
+    with open("./css/design.css") as f:
+        return f.read()
+
+#cssで装飾
+css = get_style()
+st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
+
 def main():
-    st.title("モックアップ")
+
+    with st.container():
+        st.header("モックアップ")
     status_space = st.sidebar.empty()
     status_space.subheader("動画をアップロードして下さい")
     
@@ -28,6 +38,9 @@ def main():
         #warningで使うために宣言
         line_mode = ""
         x_actual = 0
+        y_actual = 0
+        x1 = 0
+        y1 = 0
 
 
 
@@ -36,26 +49,87 @@ def main():
         # モード1の処理
             st.sidebar.write("「線」を描画して下さい")
             line_mode = st.sidebar.radio("線タイプ",["垂直","水平","その他"])
+
             if line_mode == "垂直":
                 x = st.sidebar.slider('位置', 0, 100, 50)
                 first_frame = get_first_frame(cap)
-                # resized_first_frame = convert_frame(first_frame,)
                 h, w= first_frame.shape[:2]
                 x_actual = int(w * x /100)
-                print("x_actual",x_actual)
                 first_frame = first_frame.astype(np.uint8)
-                # first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2RGB) 
                 cv2.line(first_frame, (x_actual, 0), (x_actual, h), color, thickness=2)
                 left_column.image(first_frame,use_column_width=True)
 
+            if line_mode == "水平":  
+                y = st.sidebar.slider('位置', 0, 100, 50)
+                first_frame = get_first_frame(cap)
+                h, w= first_frame.shape[:2]
+                y_actual = int(h * y /100)
+                first_frame = first_frame.astype(np.uint8)
+                cv2.line(first_frame, (0, y_actual), (w, y_actual), color, thickness=2)
+                left_column.image(first_frame,use_column_width=True)
+
+
+            if line_mode == "その他":
+                x = st.sidebar.slider('位置', 0, 100, 50)  
+                arg = st.sidebar.slider('角度', -180, 0, 95)
+                #視覚的なわかりやすさのためにこうした
+                arg += 180
+                #arg == 90 の時は垂直と同じ操作
+                if arg == 90:
+                    first_frame = get_first_frame(cap)
+                    h, w= first_frame.shape[:2]
+                    x_actual = int(w * x /100)
+                    first_frame = first_frame.astype(np.uint8)
+                    cv2.line(first_frame, (x_actual, 0), (x_actual, h), color, thickness=2)
+                    left_column.image(first_frame,use_column_width=True)
+                
+                #arg == 0,180 の時は水平と同じ操作
+                elif arg == 0 or arg == 180:
+                    first_frame = get_first_frame(cap)
+                    h, w= first_frame.shape[:2]
+                    y_actual = int(h * x /100)
+                    first_frame = first_frame.astype(np.uint8)
+                    cv2.line(first_frame, (0, y_actual), (w, y_actual), color, thickness=2)
+                    y1 = y_actual
+                    left_column.image(first_frame,use_column_width=True)
+
+                elif arg < 90:
+                    first_frame = get_first_frame(cap)
+                    h, w= first_frame.shape[:2]
+                    #実際のx切片
+                    x_actual = int(w * x /100)
+                    first_frame = first_frame.astype(np.uint8)
+                    y_intercept = int(np.tan(np.radians(arg)) * x_actual)
+                    cv2.line(first_frame, (0, y_intercept), (x_actual, 0), color, thickness=2)
+                    y1 = y_actual
+                    left_column.image(first_frame,use_column_width=True)
+
+                elif arg > 90:
+                    first_frame = get_first_frame(cap)
+                    h, w= first_frame.shape[:2]
+                    #実際のx切片
+                    x_actual = int(w * x /100)
+                    first_frame = first_frame.astype(np.uint8)
+                    y_intercept = int(-np.tan(np.radians(arg))*w+np.tan(np.radians(arg)) * x_actual)
+                    print("here")
+                    cv2.line(first_frame, (w, y_intercept), (x_actual, 0), color, thickness=2)
+                    left_column.image(first_frame,use_column_width=True)
+                    x1 = w
+                    y1 = y_intercept
+
+                print(x1)
+                print(y1)
+                # while True:
+                #     time.sleep(1)
+
+
+
+
 
                 
-            if line_mode == "水平":  
-                y = st.sidebar.slider('位置', 0, 100, disabled=True)
-            if line_mode == "その他":
-                x_move = st.sidebar.slider('x方向へ移動', 0, 100, disabled=True) 
-                y_move = st.sidebar.slider('y方向へ移動', 0, 100, disabled=True)  
-                arg = st.sidebar.slider('角度', 0, 180, disabled=True)
+
+
+                
 
         elif mode == "長方形":
         # モード2の処理
@@ -75,9 +149,21 @@ def main():
             frame = frames[i]
             resized_frame = convert_frame(frame,initial_size)
             image_space.image(resized_frame/255,use_column_width=True)
+
             if i !=0:
                 if mode == "線":
-                    warning_line(line_mode,x_actual,boxes,i ,initial_size,judge_space)
+                    if line_mode == "垂直":
+                        warning_line(line_mode,x_actual,None,boxes,i ,initial_size,judge_space,None,None)
+
+                    if line_mode == "水平":
+                        warning_line(line_mode,None,y_actual,boxes,i ,initial_size,judge_space,None,None)
+
+                    else:
+                        warning_line(line_mode,x_actual,y_actual,boxes,i ,initial_size,judge_space,x1,y1)
+
+                        
+
+
             time.sleep(0.05)
 
 
@@ -126,6 +212,7 @@ def upload_video_ui():
 
         else:
             return cap
+
 
 
 
--- warning/warning.py
+++ warning/warning.py
@@ -2,21 +2,47 @@ import numpy as np
 import streamlit as st
 import time
 
-def warning_line(line_mode,x_actual,boxes,i,initial_size,judge_space):
+def warning_line(line_mode,x_actual,y_actual,boxes,i,initial_size,judge_space,x1,y1):
     # print(initial_size)
-    if line_mode == "垂直":
-        x_scale = initial_size[1]/704
-        y_scale = initial_size[0]/704
-        box_prev = np.array([boxes[i-1][0]* x_scale, boxes[i-1][1]* y_scale,boxes[i-1][2]* x_scale,boxes[i-1][3]* y_scale])
-        box_after = np.array([boxes[i][0]* x_scale, boxes[i][1]* y_scale,boxes[i][2]* x_scale,boxes[i][3]* y_scale ])
+    x_scale = initial_size[1]/704
+    y_scale = initial_size[0]/704
+    box_prev = np.array([boxes[i-1][0]* x_scale, boxes[i-1][1]* y_scale,boxes[i-1][2]* x_scale,boxes[i-1][3]* y_scale])
+    box_after = np.array([boxes[i][0]* x_scale, boxes[i][1]* y_scale,boxes[i][2]* x_scale,boxes[i][3]* y_scale ])
 
+    if line_mode == "垂直":
         if (x_actual - (box_prev[0]+ box_prev[2]/2) )*(x_actual - (box_after[0]+box_prev[2]/2 )) <= 0:
             judge_space.write("<font color='red'>設定した線を超えました</font>", unsafe_allow_html=True)
-            time.sleep(5)
+            time.sleep(3)
         else:
             judge_space.write("異常はありません")
 
+    if line_mode == "水平":
+        if (y_actual - (box_prev[1]+ box_prev[3]/2) )*(y_actual - (box_after[1]+box_prev[3]/2 )) <= 0:
+            judge_space.write("<font color='red'>設定した線を超えました</font>", unsafe_allow_html=True)
+            time.sleep(3)
+        else:
+            judge_space.write("異常はありません") 
 
+    else:
+        middle_x_prev = box_prev[0]+ box_prev[2]/2
+        middle_y_prev = box_prev[1]+ box_prev[3]/2
+        middle_x_after = box_after[0]+box_prev[2]/2
+        middle_y_after = box_after[1]+box_prev[3]/2
 
+        if (middle_y_prev-line(x1,y1,x_actual,0,middle_x_prev))* (middle_y_after-line(x1,y1,x_actual,0,middle_x_after))<= 0:
+            judge_space.write("<font color='red'>設定した線を超えました</font>", unsafe_allow_html=True)
+            time.sleep(3)
+        else:
+            judge_space.write("異常はありません")         
+        
 
-
+def line(x1,y1,x2,y2,x):
+    print(x1)
+    print(y1)
+    print(x2)
+    print(y2)
+    if (x2-x1) != 0:
+        y = (y2-y1)*x/(x2-x1)-(y2-y1)*x1/(x2-x1) + y1
+    else:
+        y = y1 
+    return y
